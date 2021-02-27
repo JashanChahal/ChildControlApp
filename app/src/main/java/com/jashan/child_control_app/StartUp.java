bfe80788f2204d0eb49c02aea6799f4c9cf21adb
Jashan Chahal <chahaljashan123@gmail.com>
2021-02-27 21:35:51 +0530

    Added Login UI

--- app/src/main/java/com/jashan/child_control_app/StartUp.java
+++ app/src/main/java/com/jashan/child_control_app/StartUp.java
@@ -2,8 +2,11 @@ package com.jashan.child_control_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 
 import com.jashan.child_control_app.common.Login;
 
@@ -13,11 +16,16 @@ public class StartUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_up);
-//        if (isLoggedIn) {
-//            Intent intent = new Intent(this,HomePageActivity.class);
-//            startActivity(intent);
-//        } else {
-//            startActivity(new Intent(this, Login.class));
-//        }
+
+    }
+
+    public void renderLogin(View view) {
+        Intent intent = new Intent(this,Login.class);
+        Pair[] pairs = new Pair[1];
+
+        pairs[0] = new Pair<View,String>(findViewById(R.id.login_btn),"transition_login");
+        ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(this,pairs);
+
+        startActivity(intent,options.toBundle());
     }
 }
