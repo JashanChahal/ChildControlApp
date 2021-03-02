1a1a235e71d3f66d83d52460575fd72623845d33
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-02 15:29:51 +0530

    added splash screen

---
+++ app/src/main/java/com/jashan/child_control_app/activities/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.jashan.child_control_app.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.jashan.child_control_app.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, StartUp.class);
+                startActivity(intent);
+            }
+        }, SPLASH_SCREEN);
+    }
+}
