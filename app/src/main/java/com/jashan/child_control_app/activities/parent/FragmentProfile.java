00b35835de10ec6b75aa988b53ad8202141d19ff
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-02 15:15:28 +0530

    added toolbar and changed profile page UI

--- app/src/main/java/com/jashan/child_control_app/activities/parent/FragmentProfile.java
+++ app/src/main/java/com/jashan/child_control_app/activities/parent/FragmentProfile.java
@@ -1,6 +1,7 @@
 package com.jashan.child_control_app.activities.parent;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -30,24 +31,10 @@ public class FragmentProfile extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
-
-        setListenerOnLogoutButton(view);
-
         return view;
     }
 
-    private void setListenerOnLogoutButton(View view) {
-        AppCompatButton button = view.findViewById(R.id.logout_btn);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent = new Intent(getActivity(), StartUp.class);
-                startActivity(intent);
-                getActivity().finish();
-            }
-        });
-    }
+
 
     @Override
     public void onStart() {
@@ -58,9 +45,10 @@ public class FragmentProfile extends Fragment {
     }
 
     private void setProfileOfUser(WebService webService) {
+        TextView userNameTitle = getView().findViewById(R.id.username_title);
         TextView userName = getView().findViewById(R.id.profile_username);
         TextView email = getView().findViewById(R.id.profile_email);
-        TextView children = getView().findViewById(R.id.profile_children);
+//        TextView children = getView().findViewById(R.id.profile_children);
 
         ProgressBar progressBar = getView().findViewById(R.id.progressBar3);
         progressBar.setVisibility(View.VISIBLE);
@@ -72,12 +60,12 @@ public class FragmentProfile extends Fragment {
 
                 if (user != null) {
                     userName.setText(user.getUserName());
+                    userName.setTextColor(getView().getResources().getColor(R.color.black,getActivity().getTheme()));
+                    userNameTitle.setText(user.getUserName());
                     email.setText(user.getUserEmail());
-                    if (user.getType().equals("Parent"))
-                        children.setText(((Parent) user).getChildren().toString());
-                    else {
-                        children.setText("No");
-                    }
+                    email.setTextColor(getView().getResources().getColor(R.color.black,getActivity().getTheme()));
+
+
                 } else {
                     Log.e("FragmentProfile", "User is null");
                 }
--- app/src/main/java/com/jashan/child_control_app/activities/parent/ParentHomepage.java
+++ app/src/main/java/com/jashan/child_control_app/activities/parent/ParentHomepage.java
@@ -4,13 +4,17 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
-import com.jashan.child_control_app.Dashboard;
+import com.google.firebase.auth.FirebaseAuth;
 import com.jashan.child_control_app.R;
+import com.jashan.child_control_app.activities.StartUp;
 
 public class ParentHomepage extends AppCompatActivity {
 
@@ -25,27 +29,48 @@ public class ParentHomepage extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentDashboard()).commit();
 
+        setListenerOnLogoutButton();
     }
 
+    private void setListenerOnLogoutButton() {
+        TextView logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(ParentHomepage.this, StartUp.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
 
-    private  BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment =  null;
+            Fragment selectedFragment = null;
+            TextView toolbarTitle = findViewById(R.id.toolbar_title);
+            ImageView toobarIcon = findViewById(R.id.toolbar_icon);
 
             switch (item.getItemId()) {
-                case R.id.nav_dashboard :
+                case R.id.nav_dashboard:
                     selectedFragment = new FragmentDashboard();
+                    toolbarTitle.setText("Dashboard");
+                    toobarIcon.setImageResource(R.drawable.ic_baseline_dashboard);
                     break;
-                case R.id.nav_settings :
+                case R.id.nav_settings:
                     selectedFragment = new FragmentSettings();
+                    toolbarTitle.setText("Settings");
+                    toobarIcon.setImageResource(R.drawable.ic_baseline_settings);
                     break;
-                case R.id.nav_profile :
+                case R.id.nav_profile:
                     selectedFragment = new FragmentProfile();
+                    toolbarTitle.setText("Profile");
+                    toobarIcon.setImageResource(R.drawable.ic_baseline_person);
                     break;
             }
 
-            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
             return true;
         }
     };
