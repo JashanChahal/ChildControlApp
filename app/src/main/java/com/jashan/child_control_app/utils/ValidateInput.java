1358e6bf33a82af36a79bf90d31ca19de30474cc
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-12 14:42:02 +0530

    Added form validations

--- app/src/main/java/com/jashan/child_control_app/utils/ValidateInput.java
+++ app/src/main/java/com/jashan/child_control_app/utils/ValidateInput.java
@@ -1,4 +1,54 @@
 package com.jashan.child_control_app.utils;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ValidateInput {
+    public static void addEmailListener(TextInputLayout emailTextLayout) {
+        String emailRegex = "^(.+)@(.+)$";
+        Pattern emailPattern = Pattern.compile(emailRegex);
+        emailTextLayout.getEditText().addTextChangedListener(new TextValidator(emailTextLayout.getEditText(), emailTextLayout) {
+            @Override
+            public void validate(TextInputLayout layout, String text) {
+                Matcher matcher = emailPattern.matcher(text);
+                if (matcher.matches()) {
+                    layout.setError(null);
+                } else {
+                    layout.setErrorEnabled(false);
+                }
+            }
+        });
+    }
+
+    public static void addValidateListener(TextInputLayout userNameTextLayout, int minimumCharacter, String nameOfLayout) {
+        userNameTextLayout.getEditText().addTextChangedListener(new TextValidator(userNameTextLayout.getEditText(), userNameTextLayout) {
+            @Override
+            public void validate(TextInputLayout layout, String text) {
+                if (text == null || text.length() < minimumCharacter) {
+                    layout.setError(nameOfLayout + " should be atleast " + minimumCharacter + " character long");
+                } else {
+                    layout.setErrorEnabled(false);
+                }
+            }
+        });
+    }
+
+
+    public static void addMatchPasswordListener(TextInputLayout confirmPasswordTextLayout, TextInputLayout password) {
+        confirmPasswordTextLayout.getEditText().addTextChangedListener(new TextValidator(confirmPasswordTextLayout.getEditText(), confirmPasswordTextLayout) {
+            @Override
+            public void validate(TextInputLayout layout, String text) {
+                String match = password.getEditText().getText().toString();
+                if (text == null || !text.equals(match)) {
+                    layout.setError("Password donot match");
+                } else {
+                    layout.setErrorEnabled(false);
+                }
+            }
+        });
+    }
+
+
 }
