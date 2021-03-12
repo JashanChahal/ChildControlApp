834aff2d5d374460189408ca810a82cb07cb010e
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-12 14:19:52 +0530

    Added user not found exception class

--- app/src/main/java/com/jashan/child_control_app/repository/UserNotFoundException.java
+++ app/src/main/java/com/jashan/child_control_app/repository/UserNotFoundException.java
@@ -1,4 +1,7 @@
 package com.jashan.child_control_app.repository;
 
-public class UserNotFoundException {
+public class UserNotFoundException extends Exception {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
