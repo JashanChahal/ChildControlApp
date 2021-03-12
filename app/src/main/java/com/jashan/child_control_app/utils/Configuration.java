b55a2021f77d4a2c1a6a5f978590eb2743958ec3
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-12 14:18:37 +0530

    Added Configuration File

--- app/src/main/java/com/jashan/child_control_app/utils/Configuration.java
+++ app/src/main/java/com/jashan/child_control_app/utils/Configuration.java
@@ -1,4 +1,22 @@
 package com.jashan.child_control_app.utils;
 
+import com.jashan.child_control_app.repository.FirebaseWebService;
+import com.jashan.child_control_app.repository.WebService;
+
 public class Configuration {
+    private static int SPLASH_SCREEN_TIME = 2000;
+    private static WebService WEB_SERVICE = new FirebaseWebService();
+    private static String SHARED_PREFERENCE_FILE_LOCATION = "com.jashan.users";
+
+    public static WebService getWebservice() {
+        return WEB_SERVICE;
+    }
+
+    public  static  int getSplashScreenTime() {
+        return  SPLASH_SCREEN_TIME;
+    }
+
+    public static  String getSharedPreferenceFileLocation() {
+        return SHARED_PREFERENCE_FILE_LOCATION;
+    }
 }
