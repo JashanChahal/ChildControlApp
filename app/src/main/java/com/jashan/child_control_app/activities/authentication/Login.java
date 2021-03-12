fc2a15e645bca166cde8de3702edae27f89ba976
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-12 15:55:23 +0530

    Refactored login functionality (Removed Firebase Dependency)

--- app/src/main/java/com/jashan/child_control_app/activities/authentication/Login.java
+++ app/src/main/java/com/jashan/child_control_app/activities/authentication/Login.java
@@ -1,37 +1,29 @@
 package com.jashan.child_control_app.activities.authentication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
-import android.content.Intent;
+
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.jashan.child_control_app.R;
 import com.jashan.child_control_app.activities.StartUp;
-import com.jashan.child_control_app.activities.child.ChildHomepage;
-import com.jashan.child_control_app.activities.parent.ParentHomepage;
 
 import com.jashan.child_control_app.model.User;
 import com.jashan.child_control_app.repository.AfterCompletion;
-import com.jashan.child_control_app.repository.FirebaseWebService;
 import com.jashan.child_control_app.repository.WebService;
 import com.jashan.child_control_app.utils.ActivityTransition;
+import com.jashan.child_control_app.utils.Configuration;
+
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
     TextInputLayout emailTextInputLayout;
     TextInputLayout passwordTextInputLayout;
-    private FirebaseAuth mAuth;
     WebService webService;
     ProgressBar progressBar;
 
@@ -45,17 +37,11 @@ public class Login extends AppCompatActivity {
         emailTextInputLayout = findViewById(R.id.login_email);
         passwordTextInputLayout = findViewById(R.id.login_password);
 
-        mAuth = FirebaseAuth.getInstance();
         progressBar = findViewById(R.id.progressBar2);
 
         pref = getSharedPreferences("com.jashan.users", MODE_PRIVATE);
 
-        if (mAuth.getCurrentUser() != null) {
-            ActivityTransition.fetchUserDetailsAndGoToHomePage(this,progressBar,pref);
-        }
-
-
-        webService = new FirebaseWebService();
+        webService = Configuration.getWebservice();
     }
 
     public void goBackToStartUp(View view) {
@@ -73,23 +59,24 @@ public class Login extends AppCompatActivity {
     }
 
     public void login(View view) {
-        String email = emailTextInputLayout.getEditText().getText().toString();
-        String password = passwordTextInputLayout.getEditText().getText().toString();
+        String email = Objects.requireNonNull(emailTextInputLayout.getEditText()).getText().toString();
+        String password = Objects.requireNonNull(passwordTextInputLayout.getEditText()).getText().toString();
 
         progressBar.setVisibility(View.VISIBLE);
-        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this,
-                new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            ActivityTransition.fetchUserDetailsAndGoToHomePage(Login.this,progressBar,pref);
-                        } else {
-                            progressBar.setVisibility(View.GONE);
-                            Toast.makeText(Login.this, "Email or Password is wrong", Toast.LENGTH_LONG).show();
-                        }
-                    }
-                });
+
+         webService.signIn(email, password, new AfterCompletion<User>() {
+           @Override
+           public void onSuccess(User user) {
+               ActivityTransition.goToUserHomepage(Login.this,user,pref,progressBar);
+           }
+
+           @Override
+           public void onFailure(Exception e) {
+               progressBar.setVisibility(View.GONE);
+               Toast.makeText(Login.this, "Email or Password is wrong", Toast.LENGTH_LONG).show();
+           }
+       });
+
     }
 
 }
