1239eb64e2a3f6918020703905fd52d154c05469
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-15 15:57:36 +0530

    added call detail DTO

--- app/src/main/java/com/jashan/child_control_app/model/CallDetail.java
+++ app/src/main/java/com/jashan/child_control_app/model/CallDetail.java
@@ -1,4 +1,61 @@
 package com.jashan.child_control_app.model;
 
+
 public class CallDetail {
+    private String phoneNumber;
+    private String name;
+    private String date;
+    private String duration;
+
+    public CallDetail(String phoneNumber, String name, String date, String duration) {
+        this.phoneNumber = phoneNumber;
+        this.name = name == null ? "Unknown" : name;
+        this.date = date;
+        this.duration = duration;
+    }
+
+    public CallDetail() {
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? "Unkown" : name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "CallDetail{" +
+                "Phone Number='" + phoneNumber + '\'' +
+                ", Name='" + name + '\'' +
+                ", Date=" + date + '\'' +
+                ", Duration=" + duration +
+                '}';
+    }
 }
