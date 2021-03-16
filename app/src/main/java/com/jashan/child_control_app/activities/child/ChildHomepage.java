9e5b6e6abb16853b459ebd3a4db75599aaf7b15f
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-16 13:57:43 +0530

    Update database after fetching call details

--- app/src/main/java/com/jashan/child_control_app/activities/child/ChildHomepage.java
+++ app/src/main/java/com/jashan/child_control_app/activities/child/ChildHomepage.java
@@ -6,19 +6,25 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.CallLog;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.jashan.child_control_app.R;
 import com.jashan.child_control_app.activities.StartUp;
 import com.jashan.child_control_app.model.CallDetail;
+import com.jashan.child_control_app.model.Child;
+import com.jashan.child_control_app.model.ChildInformation;
+import com.jashan.child_control_app.model.Parent;
+import com.jashan.child_control_app.model.User;
+import com.jashan.child_control_app.repository.AfterCompletion;
 import com.jashan.child_control_app.repository.FirebaseWebService;
 import com.jashan.child_control_app.repository.WebService;
 
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class ChildHomepage extends AppCompatActivity {
     TextView textView;
@@ -35,21 +41,66 @@ public class ChildHomepage extends AppCompatActivity {
     }
 
     private void getCallDetails() {
+
         int LOG_COUNT = 10;
         Cursor cursor = getApplicationContext().getContentResolver().
                 query(CallLog.Calls.CONTENT_URI,null,null,null);
         cursor.moveToLast();
-
+        List<CallDetail> callDetailList = new ArrayList<>();
         while (cursor.moveToPrevious() && LOG_COUNT > 0 ) {
             String name = getCallAttribute(cursor,"name");
             String number = getCallAttribute(cursor,"formatted_number");
             String duration = getCallAttribute(cursor,"duration");
             String date = formatDate(getCallAttribute(cursor,"date"));
             CallDetail callDetail = new CallDetail(number,name,date, duration);
-            Log.i("Jashan's call", callDetail.toString());
+            callDetailList.add(callDetail);
             LOG_COUNT--;
         }
+        updateCallDetails(callDetailList);
         cursor.close();
+    }
+
+    private void updateCallDetails(List<CallDetail> callDetailList) {
+        webService.getCurrentUserAndDo(new AfterCompletion<User>() {
+            @Override
+            public void onSuccess(User user) {
+                Child child = (Child) user;
+                if (child.getChildInformation() == null) {
+                    child.setChildInformation(new ChildInformation());
+                }
+                ChildInformation childInformation =   child.getChildInformation();
+                childInformation.setCallDetails(callDetailList);
+                webService.updateUser(child);
+                updateChildInParent(child);
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
+    }
+    private void updateChildInParent(Child child) {
+        webService.queryUserByKeyValue("userEmail", child.getParentEmail(), new AfterCompletion<User>() {
+            @Override
+            public void onSuccess(User user) {
+                Parent parent = (Parent) user;
+                if (parent != null) {
+                    List<Child> children = parent.getChildren();
+                    for (int idx = 0; idx <children.size(); idx++) {
+                       if (children.get(idx).getUserEmail().equals(child.getUserEmail())){
+                           children.set(idx,child);
+                       }
+                    };
+                    webService.updateUser(parent);
+                }
+            }
+
+            @Override
+            public void onFailure(Exception e) {
+
+            }
+        });
     }
 
     private  String getCallAttribute(Cursor cursor,String query) {
