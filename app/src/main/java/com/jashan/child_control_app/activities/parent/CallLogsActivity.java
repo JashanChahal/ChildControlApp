c4d54c574f3bf61ebb25ee638ce1479165f61e76
Jashan Chahal <chahaljashan123@gmail.com>
2021-03-18 17:05:29 +0530

    Warning color added

---
+++ app/src/main/java/com/jashan/child_control_app/activities/parent/CallLogsActivity.java
@@ -0,0 +1,14 @@
+package com.jashan.child_control_app.activities.parent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CallLogsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_logs);
+    }
+}
