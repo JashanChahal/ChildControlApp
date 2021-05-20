34c0089e7c5ae4d51e3270e21f7503edd2184318
Jashan Chahal <chahaljashan123@gmail.com>
2021-05-20 14:32:10 +0530

    Created AppUsage and ScreenShot activities

---
+++ app/src/main/java/com/jashan/child_control_app/activities/parent/AppUsageActivity.java
@@ -0,0 +1,16 @@
+package com.jashan.child_control_app.activities.parent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jashan.child_control_app.R;
+
+public class AppUsageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_usage);
+    }
+}
